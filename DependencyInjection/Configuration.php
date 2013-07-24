f8af828bd1f7efe7e654990baadeef9c19a9e0eb
Marek Štípek <marek.stipek@imatic.cz>
2013-07-24 14:11:22 +0200

    RESEARCH-22: ImaticConfigBundle  - zalozeni bundlu

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,18 @@
+<?php
+namespace Imatic\Bundle\ConfigBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+
+        return $treeBuilder;
+    }
+}
---
+++ ImaticConfigBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Imatic\Bundle\ConfigBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ImaticConfigBundle extends Bundle
+{
+}
