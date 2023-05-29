10325bef1c7b36d8a65a97ce06b347e1ee73997c
Lukas Landa <landa.lukas@gmail.com>
2023-05-29 15:32:31 +0200

    PARKLANEIS-570: Attributes support

--- Entity/Config.php
+++ Entity/Config.php
@@ -1,22 +1,27 @@
 <?php declare(strict_types=1);
 namespace Imatic\Bundle\ConfigBundle\Entity;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\MappedSuperclass(repositoryClass=ConfigRepository::class)
  */
+#[ORM\MappedSuperclass(repositoryClass: ConfigRepository::class)]
 class Config
 {
     /**
      * @ORM\Id
      * @ORM\Column(type="string")
      */
+    #[ORM\Id]
+    #[ORM\Column(type: Types::STRING)]
     private ?string $key;
 
     /**
      * @ORM\Column(type="text", nullable=true)
      */
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $value;
 
     public function __construct(string $key = null, string $value = null)
